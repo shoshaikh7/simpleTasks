d5a0d48efc704eead9a924b39b42e4bf49d23c2e
Shoaib Shaikh <shoshaikh7@gmail.com>
2023-07-20 03:55:13 -0700

    restructured code to add categories to task; user can now create 5 new categories with unique name and color, and delete categories

--- index.js
+++ index.js
@@ -1,25 +1,210 @@
+// Get all HTML elements
+const greeting = document.querySelector(".greeting");
+const categoriesWrapper = document.querySelector(".categories-wrapper");
+const savedCategories = document.querySelector(".saved-categories");
+const newCategoryView = document.querySelector(".new-category");
+const newCategoryBtn = document.querySelector("#new-category-btn");
+const addCategoryBtn = document.querySelector("#add-category-btn");
+const delCategoryBtn = document.querySelector("#del-category-btn");
+const categoryInput = document.querySelector("#category-input");
+const categoryColors = document.querySelector(".category-colors");
+const newTaskBtn = document.querySelector("#new-task-btn");
+// Global tasks and categories variable
+const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+const categories = JSON.parse(localStorage.getItem("categories")) || [];
+// Grab username from local storage or set to empty
+const username = localStorage.getItem("username" || "");
+const nameInput = document.querySelector("#name");
+
 // Initialize app
 const init = () => {
-  // Global tasks variable
-  tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  // Grab username from local storage or set to empty
-  const username = localStorage.getItem("username" || "");
-  const nameInput = document.querySelector("#name");
-  // Grab all HTML elements
-  const newTaskForm = document.querySelector("#new-task-form");
-  const newTaskBtn = document.querySelector("#new-task-btn");
-  // Create new todo
+  console.log("called init");
+  // Add event listeners to all buttons
+  nameInput.addEventListener("blur", updateGreeting);
+  newCategoryBtn.addEventListener("click", showCategoryForm);
+  addCategoryBtn.addEventListener("click", addNewCategory);
+  delCategoryBtn.addEventListener("click", deleteCategory);
   newTaskBtn.addEventListener("click", addNewTask);
 };
 
-const addNewTask = (e) => {
-  // Prevent default action on form
-  e.preventDefault();
+const updateGreeting = () => {
+  console.log("called updateGreeting");
+  if (username !== null) {
+    greeting.firstElementChild.textContent = `Hello, ${username}!`
+  } else {
+    greeting.firstElementChild.textContent = `Hello, ${nameInput.value}!`
+  }
+};
+
+const showCategoryForm = () => {
+  console.log("called showCategoryForm");
+  // Show category input and colors
+  if (newCategoryView.style.display === "none") {
+    newCategoryView.style.display = "block";
+    addCategoryBtn.style.display = "block";
+    delCategoryBtn.style.display = "block";
+    newCategoryBtn.style.display = "none";
+    newTaskBtn.style.display = "none";
+  }
+};
+
+const hideCategoryForm = () => {
+  console.log("called hideCategoryForm");
+  newCategoryView.style.display = "none";
+  addCategoryBtn.style.display = "none";
+  delCategoryBtn.style.display = "none";
+  newCategoryBtn.style.display = "block";
+  newTaskBtn.style.display = "block";
+};
+
+const addNewCategory = () => {
+  console.log("called addNewCategory");
+  // Get selected color value and checked input radio
+  let { selectedColorVal, checkedColorInput } = getSelectedColor();
+  let catValue = categoryInput.value;
+  let existingCat = false;
+  // Check if catValue !== already existing cat value
+  for (const category of categories) {
+    if (catValue === category.content) {
+      existingCat = true;
+      console.log(existingCat);
+    }
+  }
+  // Create a new category item
+  const categoryItem = {
+    id: Math.random() * 1000,
+    content: "",
+    color: "",
+    createdAt: new Date().getTime(),
+  };
+  // Check to see if input value is empty
+  if (catValue === "" || existingCat) {
+    alert("Please enter a unique name for your category");
+    showCategoryForm();
+  } else {
+    categoryItem.content = catValue;
+  }
+  // Check to see if color value is empty
+  if (selectedColorVal === "") {
+    alert("Please select a color for your category");
+    showCategoryForm();
+  } else {
+    categoryItem.color = selectedColorVal;
+  }
+  // If everything is okay, do this
+  if (categoryItem.content !== "" && categoryItem.color !== "") {
+    // Uncheck, disable, and hide used color
+    let color = checkedColorInput[0];
+    color.checked = false;
+    color.disabled = true;
+    if (color.disabled) {
+      color.parentElement.style.display = "none";
+    }
+    // Add the item to the front of the categories array
+    categories.unshift(categoryItem);
+    // Pass categoryItem to createCategoryElements to create HTML and store in catElements
+    const catElements = createCategoryElements(categoryItem);
+    // Append catElements to savedCategories
+    savedCategories.append(catElements);
+    // Hide newCategoryForm
+    hideCategoryForm();
+    // Check if categories reached max limit of 5
+    if (categories.length >= 5) {
+      delCategoryBtn.style.display = "block";
+      newCategoryBtn.style.display = "none";
+    }
+    //  Reset input value once collected and saved
+    categoryInput.value = "";
+  }
+};
+
+const getSelectedColor = () => {
+  console.log("calledGetSelectedColor");
+  let selectedColorVal = "";
+  const colorOptions = categoryColors.querySelectorAll("input");
+  const checkedColorInput = Array.from(colorOptions).filter((color) => {
+    if (color.checked) {
+      selectedColorVal = color.value;
+      return color;
+    }
+  });
+  return { selectedColorVal, checkedColorInput };
+}
+
+const createCategoryElements = (categoryItem) => {
+  console.log("called createCategoryElements");
+  // Create HTML elements, add classes and attributes for each category item
+  let catContent = categoryItem.content;
+  let catColor = categoryItem.color;
+  const catLabel = document.createElement("label");
+  catLabel.setAttribute("for", catContent);
+
+  const catCheckboxBubble = document.createElement("input");
+  catCheckboxBubble.type = "radio";
+  catCheckboxBubble.setAttribute("name", "category");
+  catCheckboxBubble.setAttribute("class", catColor);
+  catCheckboxBubble.setAttribute("id", catContent);
+  catCheckboxBubble.value = catContent;
+
+  const catBubble = document.createElement("span");
+  catBubble.setAttribute("class", "bubble " + catColor);
+
+  const catName = document.createElement("p");
+  catName.classList.add("category-name");
+  catName.textContent = catContent;
+  // Append everything to the catLabel
+  catLabel.append(catCheckboxBubble, catBubble, catName);
+  return catLabel;
+};
+
+const deleteCategory = () => {
+  console.log("called deleteCategory");
+  // Store checked input inside selectedCat
+  let cats = savedCategories.getElementsByTagName("input");
+  let selectedCat = "";
+  for (let i = 0; i < cats.length; i++) {
+    let cat = cats[i];
+    if (cat.checked) {
+      selectedCat = cat;
+    }
+  }
+  // Check if cat is selected
+  if (selectedCat === "") {
+    alert("Please select an existing category to delete");
+  } else {
+    let confirmed = confirm(
+      `This will remove ${selectedCat.id} and all tasks associated with it. Are you sure you want to delete this category?`
+    );
+    if (confirmed) {
+      selectedCat.parentElement.remove();
+      // Check to see which category item has the same id as the selectedCatId
+      for (let i = categories.length - 1; i >= 0; --i) {
+        if (categories[i].content === selectedCat.id) {
+          categories.splice(i, 1);
+        }
+      }
+      // Add category color back into options
+      const colorOptions = categoryColors.querySelectorAll("input");
+      for (let i = 0; i < colorOptions.length; i++) {
+        let color = colorOptions[i];
+        console.log(color.id, selectedCat.className);
+        if (color.id.trim() === selectedCat.className.trim()) {
+          console.log("match")
+          console.log(color);
+          color.parentElement.style.display = "flex";
+          color.disabled = false;
+        }
+      }
+    }
+  }
+};
+
+const addNewTask = () => {
   // Create a new task item
   const taskItem = {
-    id: Math.random() * 1000000,
+    id: Math.random() * 1000,
     content: "",
-    cat: "",
+    category: "",
     completed: false,
     createdAt: new Date().getTime(),
   };
@@ -50,7 +235,7 @@ const createTaskElements = (taskItem) => {
   taskContent.classList.add("task-content");
   taskContent.type = "text";
   taskContent.value = taskItem.content;
-  taskContent.setAttribute("disabled", "")
+  taskContent.setAttribute("disabled", "");
 
   const actions = document.createElement("div");
   actions.classList.add("actions");
@@ -63,55 +248,53 @@ const createTaskElements = (taskItem) => {
   deleteBtn.classList.add("delete-btn");
   deleteBtn.innerHTML = "Delete";
 
-
   if (todo.category == "personal") {
     span.classList.add("personal");
   } else {
     span.classList.add("business");
   }
-  if ()
 
   task.innerHTML = `<input type="text" value="${todo.task}" disabled />`;
 };
 
 // Grab saved data from local storage if there is any
-window.addEventListener("load", () => {
-  // Get input value and set it to username in local storage
-  // nameInput.value = username;
-  // nameInput.addEventListener("change", (e) => {
-  //   localStorage.setItem("username", e.target.value);
-  // });
+// window.addEventListener("load", () => {
+//   // Get input value and set it to username in local storage
+//   // nameInput.value = username;
+//   // nameInput.addEventListener("change", (e) => {
+//   //   localStorage.setItem("username", e.target.value);
+//   // });
 
-  // Add new todo
-  newToDoForm.addEventListener("submit", (e) => {
-    // Stop default action from submit button
-    e.preventDefault();
+//   // Add new todo
+//   newToDoForm.addEventListener("submit", (e) => {
+//     // Stop default action from submit button
+//     e.preventDefault();
 
-    // Create todo object, get values from input in form and set to each property
-    const todo = {
-      task: e.target.elements.task.value,
-      category: e.target.elements.category.value,
-      done: false,
-      createdAt: new Date().getTime(),
-    };
+//     // Create todo object, get values from input in form and set to each property
+//     const todo = {
+//       task: e.target.elements.task.value,
+//       category: e.target.elements.category.value,
+//       done: false,
+//       createdAt: new Date().getTime(),
+//     };
 
-    // Add each todo to the todos array
-    todos.push(todo);
+//     // Add each todo to the todos array
+//     todos.push(todo);
 
-    // Convert todos array into JSON and save to local storage variable called todos
-    localStorage.setItem("todos", JSON.stringify(todos));
+//     // Convert todos array into JSON and save to local storage variable called todos
+//     localStorage.setItem("todos", JSON.stringify(todos));
 
-    // Reset form
-    e.target.reset();
+//     // Reset form
+//     e.target.reset();
 
-    // Create todos
-    createTodos();
+//     // Create todos
+//     createTodos();
 
-    // Check if todos are done
-    // checkTodos();
-  });
-  createTodos();
-});
+//     // Check if todos are done
+//     // checkTodos();
+//   });
+//   createTodos();
+// });
 
 // Create todos
 const createTodos = () => {
